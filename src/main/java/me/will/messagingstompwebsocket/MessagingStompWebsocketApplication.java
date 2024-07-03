0b811e995825c41e159eaef1052593a21a33b839
Will <yzj_000@163.com>
2024-07-03 20:41:02 +0800

    Initial commit

---
+++ src/main/java/me/will/messagingstompwebsocket/MessagingStompWebsocketApplication.java
@@ -0,0 +1,13 @@
+package me.will.messagingstompwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingStompWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagingStompWebsocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/will/messagingstompwebsocket/MessagingStompWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package me.will.messagingstompwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MessagingStompWebsocketApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
