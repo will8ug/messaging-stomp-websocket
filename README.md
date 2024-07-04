7734d11f6b167c6e5d46404e8efaf38c02d523e0
Will <yzj_000@163.com>
2024-07-04 20:02:47 +0800

    A bit more tests of those STOMP COMMANDS

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # messaging-stomp-websocket
+
+Visit http://localhost:8080 to start testing.
--- src/main/java/me/will/messagingstompwebsocket/WebSocketConfig.java
+++ src/main/java/me/will/messagingstompwebsocket/WebSocketConfig.java
@@ -1,7 +1,13 @@
 package me.will.messagingstompwebsocket;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -18,6 +24,18 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ms-websocket");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+                System.out.println(accessor.getCommand() + ": " + message.getPayload());
+                return ChannelInterceptor.super.preSend(message, channel);
+            }
+        });
     }
 
 }
